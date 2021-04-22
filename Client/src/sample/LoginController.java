3bfb0ca581995d905f012616d4e7a23802345593
esteban.moll@free.fr <esteban.moll@free.fr>
2021-04-22 17:24:59 +0200

    Front page register OK

--- Client/src/sample/LoginController.java
+++ Client/src/sample/LoginController.java
@@ -2,18 +2,29 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
     @FXML
     private Button cancelButton;
 
-    public void cancelButtonOnAction(ActionEvent event)
+    public void registerHyperlinkPushed(ActionEvent event) throws IOException
     {
-        Stage stage = (Stage) cancelButton.getScene().getWindow();
-        stage.close();
+        Parent registerParent = FXMLLoader.load(getClass().getResource("register.fxml"));
+        Scene registerScene = new Scene(registerParent);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(registerScene);
+        window.show();
     }
 
 }
---
+++ Client/src/sample/RegisterController.java
@@ -0,0 +1,53 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegisterController {
+
+    @FXML private Spinner PoidsSpinner;
+
+    @FXML private Spinner TailleSpinner;
+
+    @FXML private ComboBox SexeComboBox;
+
+    @FXML private ComboBox NiveauComboBox;
+
+    @FXML private Button CreateAccountButton;
+
+    public void createAccountButtonPushed(ActionEvent event) throws IOException
+    {
+        Parent loginParent = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene loginScene = new Scene(loginParent);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(loginScene);
+        window.show();
+    }
+
+    @FXML
+    public void initialize()
+    {
+        SpinnerValueFactory<Integer> listPoids = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,200,70);
+        this.PoidsSpinner.setValueFactory(listPoids);
+
+        SpinnerValueFactory<Integer> listTaille = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,250,170);
+        this.TailleSpinner.setValueFactory(listTaille);
+
+        this.SexeComboBox.getItems().addAll("Homme", "Femme", "Autre");
+
+        this.NiveauComboBox.getItems().addAll("Débutant", "Intermédiaire", "Confirmé", "Expert", "Alpha");
+    }
+}
