2a5900871124491598400d8c0ce514496c450795
esteban.moll@free.fr <esteban.moll@free.fr>
2021-04-27 11:10:32 +0200

    Hash du mdp avant envoi des données au serveur

--- Client/src/Controller/RegisterController.java
+++ Client/src/Controller/RegisterController.java
@@ -11,6 +11,10 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -33,11 +37,13 @@ public class RegisterController {
     @FXML private Label errorPwd;
     @FXML private Label errorConfirmPwd;
 
-    public void createAccountButtonPushed(ActionEvent event) throws IOException
-    {
+    public void createAccountButtonPushed(ActionEvent event) throws IOException, NoSuchAlgorithmException {
         if(checkPwd()) {
             if (AlphActivity.client.connect()) {
-                if (AlphActivity.client.createAccount(usernameTextField.getText(), enterPasswordField.getText(), textCountry.getText(), textRegion.getText(), textCity.getText(), (int) PoidsSpinner.getValue(), birthdate.getValue().toString(), (int) TailleSpinner.getValue(), SexeComboBox.getValue().toString(), NiveauComboBox.getValue().toString())) {
+
+                String pwdHash = hash256(enterPasswordField);
+
+                if (AlphActivity.client.createAccount(usernameTextField.getText(), pwdHash, textCountry.getText(), textRegion.getText(), textCity.getText(), (int) PoidsSpinner.getValue(), birthdate.getValue().toString(), (int) TailleSpinner.getValue(), SexeComboBox.getValue().toString(), NiveauComboBox.getValue().toString())) {
                     Parent loginParent = FXMLLoader.load(getClass().getResource("../View/login.fxml"));
                     Scene loginScene = new Scene(loginParent);
 
@@ -107,5 +113,38 @@ public class RegisterController {
             errorPwd.setVisible(true);
             return false;
         }
+    }
+
+    private String hash256(PasswordField pf) throws NoSuchAlgorithmException
+    {
+        return toHexString(getSHA(pf.getText()));
+    }
+
+    public static byte[] getSHA(String input) throws NoSuchAlgorithmException
+    {
+        // Static getInstance method is called with hashing SHA
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        // digest() method called
+        // to calculate message digest of an input
+        // and return array of byte
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static String toHexString(byte[] hash)
+    {
+        // Convert byte array into signum representation
+        BigInteger number = new BigInteger(1, hash);
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 32)
+        {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
     }
 }
